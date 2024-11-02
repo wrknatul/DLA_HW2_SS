630ddcfc2bf7f1e31a11162148fdd4f312585ec3
wrknatul <wrk.natul@yandex.ru>
2024-11-02 03:19:14 +0300

    FUCK(

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -36,7 +36,7 @@ class Trainer(BaseTrainer):
 
         outputs = self.model(**batch)
         batch.update(outputs)
-        batch["labels"] = torch.Tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        batch["labels"] = torch.ones((10, 10))/10
         all_losses = self.criterion(**batch)
         batch.update(all_losses)
 
